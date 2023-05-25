ad0bde7bdecf6b47da4057c8ddb6ada177aed008
Bryan Guerra <bryanmlg2018@gmail.com>
2023-05-25 12:20:38 -0600

    fix editform

--- src/pages/dashboard/edit/[id].js
+++ src/pages/dashboard/edit/[id].js
@@ -1,6 +1,6 @@
 import EditItem from '@services/editItem';
 import EditForm from '@components/EditForm';
 export default function Edit() {
-  const product = EditItem();
-  return <EditForm product={product}/>;
+  const item = EditItem();
+  return <EditForm item={item}/>;
 }
